a542a41c5ed81cb883c514aebb2be99481faf640
lawsondaniel <lawblaze4@gmail.com>
2022-06-29 22:13:46 +0100

    done with loggin what is left is jwt

--- routes/Login.js
+++ routes/Login.js
@@ -1,11 +1,48 @@
 import  express  from "express";
+import usersSchema from "../models/usersSchema.js";
+import bcrypt from 'bcrypt'
 const LoginRouter = express.Router()
 
 //create user login route
 
 LoginRouter.post("/",(req,res)=>{
-    res.send("login")
-    
+    const email = req.body.email
+    const password = req.body.password
+
+    //get the user trying to login
+    usersSchema.findOne({
+        email:email
+    }) .then((value)=>{
+        
+        //check if the user exists
+        if(value === null){
+            res.status(404).json({
+                error:"username does not exists"
+            })
+        }else{
+            //compare password with hashed value
+            console.log(password)
+            console.log(value.password)
+            const isValidPass = bcrypt.compareSync(password, value.password);
+            if(isValidPass){
+                res.json({
+                    message:"Users logged in sucessfully",
+                    value:value
+                })
+            }else{
+                res.json({
+                    err:"wrong passord"
+                })
+            }
+
+        
+        }
+    }).catch((err)=>{
+        res.json({
+            message:err,
+            
+        })
+    })
 })
 
 export default LoginRouter
